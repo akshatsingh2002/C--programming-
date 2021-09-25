ff89a101f3f7bf6c9ceea0d2b6eb8e9a970d6fcc
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2021-09-25 11:51:41 +0530

    commit

---
+++ opps2.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class Employee{   //by default all thing are private in calss//
+//we need to use access modifier to make it public//
+public: 
+    string Name;
+    string Company;
+    int Age;
+    void Introduction(){
+        cout<<"Name="<<Name<<endl;
+        cout<<"Age="<<Age<<endl;
+        cout<<"Company="<<Company<<endl;
+    }
+Employee(string name,string company,int age)
+{//creating a constructor//
+
+Name=name;
+Company=company;
+Age=age;
+}
+};//this just represents structure not data//
+
+
+
+int main(){
+//creating instance in the main funcion//
+int number;
+/*but
+if we have to enter it for many people  we have write the code many time 
+which is idiotic so we will use contructors*/
+/*even if we dont create constructors system automactically creates a 
+constructor that is called a default constructor it is a method but it 
+still does not have any return type*/
+
+Employee employee3=Employee("shiv","amazon",35);
+//using constructor to enter the data//
+employee3.Introduction();
+
+    return 0;
+}

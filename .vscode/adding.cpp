33d0370aac717e494b07fb26051e2cfe7eb96cb7
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2021-10-08 14:15:24 +0530

    commit

---
+++ .vscode/adding.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class add{
+    public:
+    float num1,num2;
+    void get_data(float a, float b){
+        num1=a;
+        num2=b;
+    }
+        void multiplying(){
+            cout<<num1*num2;;
+        }
+        void adding(){
+            cout<<(int)(num1+num2);
+            }
+
+    
+};
+int main(){
+    add a1;
+    float x,y;
+    cout<<"Enter the number you want to add and multiply"<<endl;
+    cin>>x>>y;
+    a1.get_data(x,y);
+    a1.multiplying();
+    cout<<endl;
+    a1.adding();
+
+    return 0;
+}
